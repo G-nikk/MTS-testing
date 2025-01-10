a66eb058ab8ca9bdb6749667fcf5e9d7fc005ed0
German <gnshibanov@mail.ru>
2025-01-10 11:23:32 +0300

    first commit

---
+++ src/main/java/com/example/mts_testing/MtsTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mts_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtsTestingApplication.class, args);
+	}
+
+}
